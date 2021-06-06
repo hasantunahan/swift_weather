43ddeadb73e142413c18ef898a33eda4be12c0c9
hasantunahan <hasan.tunahan.ak@gazi.edu.tr>
2021-06-06 14:50:27 +0300

    Initial Commit

---
+++ weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather
+//
+//  Created by HasanTunahan on 6.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by HasanTunahan on 6.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
